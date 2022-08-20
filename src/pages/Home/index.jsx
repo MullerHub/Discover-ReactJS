d70517d8185855b7b439e5f12209c9c33082d39d
murilo muller <murilomuller@protonmail.com>
2022-08-20 17:20:48 -0300

    API adicionada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,67 @@
 import './styles.css'
+import React, {useState, useEffect} from 'react'
 import {Card} from '../../Components/Card'
 
 export function Home() {
 
+  const [studentName, setStudentName] = useState()
+  const [students, setStudents] = useState([])
+  const [user, setUser] = useState({ name: '', avatar: ''})
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    }
+
+    setStudents(prevState => [...prevState, newStudent])
+  }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/mullerhub")
+    .then((response) => response.json())
+    .then((data) => {
+      setUser({
+      name: data.name,
+      avatar: data.avatar_url,
+    })
+    })
+  }, []);
+
   return (
     <div className='container'>
-      <h1>Lista de Presença</h1>
-      <input type='text' placeholder="Digite o nome..." />
-      <button type="button" >Adicionar</button>
-      <Card name="Murilo" time="10:55:25" />
-      <Card name="Hortencio" time="11:00:10" />
-      <Card name="Rafaela" time="12:10:33" />
-    </div>
+      <header>
+        <h1>Lista de presenças</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="MullerHub image" />
+         </div>
+      </header>
 
+      <input 
+      type='text' 
+      placeholder="Digite o nome..."
+      onChange={(e) => setStudentName(e.target.value)}
+       />
+
+      <button 
+      type="button" 
+      onClick={handleAddStudent}>
+      Adicionar
+      </button>
+      
+      {
+        students.map((student) => (
+          <Card 
+            key={student.time}  
+            name={student.name} 
+            time={student.time} 
+          />
+      ))}
+    </div>
   )
 }
