90acbe0bb3a40eda90fc2c84740a39bcbe01a19b
murilo muller <murilomuller@protonmail.com>
2022-10-26 22:40:55 -0300

    Chore: add Inserction of types

--- Typescript/index2.ts
+++ Typescript/index2.ts
@@ -14,3 +14,24 @@ let newUser2: UserOptional = {
   email: 'joao@email.com',
   age: 18,
 }
+
+// Insercção de tipos
+
+type UserInsert = {
+  id: number,
+  name: string,
+}
+
+type Char = {
+  nickname: string,
+  level: number
+}
+
+type PlayerInfo = UserInsert & Char;
+
+let info: PlayerInfo = {
+  id: 1,
+  name: 'João Inácio',
+  nickname: 'birobirobiro',
+  level: 50
+}
