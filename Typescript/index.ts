ba1412f55b483db99dc56286847d17d98e6194fd
murilo muller <murilomuller@protonmail.com>
2022-10-26 22:17:52 -0300

    Chore: add types with object

--- Typescript/index.ts
+++ Typescript/index.ts
@@ -42,3 +42,17 @@ type UserResponse = {
 }
 
 let userResponse = {} as UserResponse;
+
+// Type com objetos
+
+type Point = {
+  x: number;
+  y: number;
+}
+
+function printCoord(points: Point) {
+  console.log(`O eixo x é: ${points.x}`)
+  console.log(`O eixo y é: ${points.y}`)
+}
+
+printCoord({x: 101, y: 50})
