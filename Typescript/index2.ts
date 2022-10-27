477512761c573db04294735cac188a86e054042d
murilo muller <murilomuller@protonmail.com>
2022-10-26 22:35:46 -0300

    Refactor: ...

--- Typescript/index2.ts
+++ Typescript/index2.ts
@@ -1,7 +1,7 @@
 // tipagem com objetos opcional
 
 
-type User2 = {
+type UserOptional = {
   name: string;
   email: string;
   age: number;
@@ -9,9 +9,8 @@ type User2 = {
   // isAdmin não será obrigatória na sua declaração
 }
 
-let newUser2: User = {
+let newUser2: UserOptional = {
   name: 'João',
   email: 'joao@email.com',
   age: 18,
-  // isAdmin: true    NÃO É NECESSARIO USAR POR SER UM TIPO OPCIONAL NA TIPAGEM COM O USO DO "?"
 }
